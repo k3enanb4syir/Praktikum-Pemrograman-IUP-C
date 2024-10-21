86a27532fcb6fc4978d8e3f02ed6642de7d73195
Muhammad Keenan Basyir <mouchbobby@gmail.com>
2024-10-21 20:59:36 +0700

    Add files via upload

---
+++ meeting7/problem1/meeting7_problem1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void add(int a, int b) {
+    cout << "Answer = " << a << "+" << b << " = " << a+b << endl;
+}
+void subtract(int a, int b) {
+    cout << "Answer = " << a << "-" << b << " = " << a-b << endl;
+}
+
+
+int main()
+{
+    char operation;
+    int a;
+    int b;
+    
+    cout << "Enter first number" << endl;
+    cin >> a;
+    cout << "Enter an operation (+ or -)" << endl;
+    cin >> operation;
+    cout << "Enter second number" << endl;
+    cin >> b;
+    
+    if (operation == '+') {
+        add(a,b);
+    }
+    else{
+        subtract(a,b);
+    }
+    
+    
+    
+}
