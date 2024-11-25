e1eb915138d42cceea7212da939910f06ac64e12
Muhammad Keenan Basyir <mouchbobby@gmail.com>
2024-11-25 15:53:38 +0700

    Add files via upload

---
+++ UAS_programming/problem1/finalexam_problem1_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //find area of circle MAKETEST
+using namespace std;
+
+int main()
+{
+    float PI = 3.14159;
+    float r = 4;
+    float radius;
+    float area;
+    
+    cout << "Finding area of circle" << endl;
+    cout << "Value of PI = " << PI << endl;
+    
+    radius = r*r;
+    area = radius*PI;
+    
+    cout << "Area of circle = " << area << endl;
+    
+    
+    
+}
